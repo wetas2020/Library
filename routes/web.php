ecdc19e01370a818f8b749347fe06cff3a97336e
wetas2020 <62056784+wetas2020@users.noreply.github.com>
2021-12-01 18:38:07 +0100

    Test Driven - Books Test Part 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/books', [App\Http\Controllers\BooksController::class, 'store']);
+Route::patch('/books/{book}', [App\Http\Controllers\BooksController::class, 'update']);
